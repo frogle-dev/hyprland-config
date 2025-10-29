f1bff3157c6584bec3c6efb27bb2b5d080068ba3
frogle-dev <jonah0602@outlook.com>
2025-10-29 08:01:54 -0700

    config folders

---
+++ copy_config.sh
@@ -0,0 +1,8 @@
+#1/bin/sh
+
+cp -r ~/.config/walker config/walker
+cp -r ~/.config/hypr config/hypr
+cp -r ~/.config/waybar config/waybar
+cp -r ~/.config/kitty config/kitty
+cp -r ~/.config/nvim config/nvim
+cp -r ~/.config/waypaper config/waypaper
