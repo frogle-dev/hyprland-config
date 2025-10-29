05d36f49ba68e8c7768b7e23a0993a1b25a3e650
frogle-dev <jonah0602@outlook.com>
2025-10-29 08:15:49 -0700

    config folder 2

--- copy_config.sh
+++ copy_config.sh
@@ -6,3 +6,7 @@ cp -r ~/.config/waybar config/waybar
 cp -r ~/.config/kitty config/kitty
 cp -r ~/.config/nvim config/nvim
 cp -r ~/.config/waypaper config/waypaper
+cp -r ~/.oh-my-zsh config/oh-my-zsh
+cp ~/.zshrc config
+
+find config -name ".git*" | xargs rm -r 
